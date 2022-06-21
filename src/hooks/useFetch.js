15489eccfbc0cae031a371f6679cd932afce0bf4
anuzza <anuja3580@gmail.com>
2022-06-21 05:28:53 -0500

    used the custom fetch hook

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,24 +1,29 @@
 
 import react ,{useState, useEffect} from "react";
 
+
 const useFetch = (requestConfig, applyData)=>{
     
 const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
+const [error, setError] = useState(null);
 
   const sendRequest = async () => {
     setIsLoading(true);
     setError(null);
     try {
       const response = await fetch(
-        requestConfig.url, {method: requestConfig.method,
-        headers: requestConfig.headers,
-    body: JSON.stringify(requestConfig.body)}
-      );
+        requestConfig.url, {
+            method: requestConfig.method ? requestConfig.method: 'GET' ,
+
+            headers: requestConfig.headers? requestConfig.headers :{},
+        
+            body: requestConfig.body?  JSON.stringify(requestConfig.body) : null,
+    
+        });
 
       if (!response.ok) {
         throw new Error('Request failed!');
-      }
+        }
 
       const data = await response.json();
       applyData(data);
