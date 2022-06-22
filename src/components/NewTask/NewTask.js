87cebd62d2d1d3a79f1100b963ea99c319daddea
anuzza <anuja3580@gmail.com>
2022-06-22 02:39:59 -0500

    used custom hooks in newtask

--- src/components/NewTask/NewTask.js
+++ src/components/NewTask/NewTask.js
@@ -2,41 +2,34 @@ import { useState } from 'react';
 
 import Section from '../UI/Section';
 import TaskForm from './TaskForm';
+import useFetch from '../../hooks/useFetch';
 
 const NewTask = (props) => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
 
-  const enterTaskHandler = async (taskText) => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(
-        'https://react-http-76fdb-default-rtdb.firebaseio.com//tasks.json',
-        {
-          method: 'POST',
-          body: JSON.stringify({ text: taskText }),
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        }
-      );
+  const {isLoading, error, sendRequest: sendTaskRequest }= useFetch();
 
-      if (!response.ok) {
-        throw new Error('Request failed!');
-      }
-
-      const data = await response.json();
-
-      const generatedId = data.name; // firebase-specific => "name" contains generated id
+  const createTask=(taskText, taskData)=>{
+    const generatedId = taskData.name; // firebase-specific => "name" contains generated id
       const createdTask = { id: generatedId, text: taskText };
 
       props.onAddTask(createdTask);
-    } catch (err) {
-      setError(err.message || 'Something went wrong!');
-    }
-    setIsLoading(false);
-  };
+
+
+  }
+  
+
+  const enterTaskHandler = async (taskText) => {
+    sendTaskRequest({
+      url: 'https://react-http-76fdb-default-rtdb.firebaseio.com//tasks.json',
+      method: 'POST',
+      headers: {
+            'Content-Type': 'application/json',
+          },
+      body:{ text: taskText },
+
+  }, createTask.bind(null, taskText));
+    
+};
 
   return (
     <Section>
